f6764796e85592c4ee7b119321f42a6b347a77d8
devsamahd <devsamahd@gmail.com>
2023-03-04 00:42:09 +0100

    ...

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const PORT = process.env.PORT || 310
 DB()
 App.use(cors())
 
-
 App.use(express.urlencoded({extended: false}))
 App.use(express.json())
 
 App.get('/', (req, res)=>{return res.status(200).json({message:"server is running, Hurray!"})})
 App.use('/generateCode',require('./routes/code.routes'))
-App.use('/org',require('./routes/code.routes'))
+App.use('/org',require('./routes/org.routes'))
 
 mongoose.connection.once('open',()=>{
     App.listen(PORT, ()=>{ console.log('connected') })
