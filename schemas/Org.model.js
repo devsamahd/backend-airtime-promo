56f5e5856642d08a6ba68da75259afaccadd1df2
devsamahd <devsamahd@gmail.com>
2023-03-04 00:42:29 +0100

    ...

--- schemas/Org.model.js
+++ schemas/Org.model.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose')
 
 const orgSchema = new mongoose.Schema({
         orgName: {type:String, required:true},
+},{
+        timestamps:true,
+        toJSON:{virtuals:true},
+        toObject:{virtuals:true}
 })
+
+orgSchema.virtual('codeCount',{
+        ref:'codes',
+        foreignField: 'orgId',
+        localField: '_id',
+        count: true
+})
+
 
 module.exports = mongoose.model('orgs', orgSchema)
